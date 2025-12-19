6139d86c51ef81bcc2340ece4f4a228d33260b45
dreamo199 <alagbaosokelvin@gmail.com>
2025-12-19 22:49:04 +0100

    Third Commit

--- MovieAPI/Movieapi/Movieapi/settings.py
+++ MovieAPI/Movieapi/Movieapi/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^m87olcxgtdel7o&&p6^yz*5)*+&-jc#tv4ff3$i*g%4+^1pph'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
